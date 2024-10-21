3d9fe58ac651551985e8c18a8dcb54ea0af04d16
igerrits13 <77903575+igerrits13@users.noreply.github.com>
2024-10-20 22:48:01 -0600

    Add in type icons

--- pokeapi_react_app/src/components/GenHero.js
+++ pokeapi_react_app/src/components/GenHero.js
@@ -1,26 +1,35 @@
+import ElectricType from "./icons/electric.svg";
+import GrassType from "./icons/grass.svg";
+import FireType from "./icons/fire.svg";
+import WaterType from "./icons/water.svg";
+
 const GenHero = () => {
   return (
     <div className="container d-flex justify-content-center">
       <div className="row w-100">
-        <div className="col genLeft opacity-50 h-50">
+        <div className="col genLeft opacity-50 h-50 genImgBox">
+          <img className="electricType" src={ElectricType}></img>
           <img
             className="img-fluid genImg"
             src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
           ></img>
         </div>
-        <div className="col genMid opacity-50 h-50">
+        <div className="col genMid opacity-50 h-50 genImgBox">
+          <img className="grassType" src={GrassType}></img>
           <img
             className="img-fluid genImg"
             src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
           ></img>
         </div>
-        <div className="col genMid2 opacity-50 h-50">
+        <div className="col genMid2 opacity-50 h-50 genImgBox">
+          <img className="fireType" src={FireType}></img>
           <img
             className="img-fluid genImg"
             src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"
           ></img>
         </div>
-        <div className="col genRight opacity-50 h-50">
+        <div className="col genRight opacity-50 h-50 genImgBox">
+          <img className="waterType" src={WaterType}></img>
           <img
             className="img-fluid genImg"
             src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png"
