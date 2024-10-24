0549d3439dc138afd13974bb17013443ccb5fb13
igerrits13 <77903575+igerrits13@users.noreply.github.com>
2024-10-24 12:38:28 -0600

    Cleanup un-needed bootstrap dark mode code

--- pokeapi_react_app/src/App.js
+++ pokeapi_react_app/src/App.js
@@ -13,7 +13,6 @@ function App() {
 
   (() => {
     const getStoredTheme = () => localStorage.getItem("theme");
-    // const setStoredTheme = (theme) => localStorage.setItem("theme", theme);
 
     const getPreferredTheme = () => {
       const storedTheme = getStoredTheme();
@@ -40,47 +39,6 @@ function App() {
     };
 
     setTheme(getPreferredTheme());
-
-    // const showActiveTheme = (theme, focus = false) => {
-    //   const themeSwitcher = document.querySelector("#bd-theme");
-
-    //   if (!themeSwitcher) {
-    //     return;
-    //   }
-
-    //   const themeSwitcherText = document.querySelector("#bd-theme-text");
-    //   const activeThemeIcon = document.querySelector(".theme-icon-active use");
-    //   const btnToActive = document.querySelector(
-    //     `[data-bs-theme-value="${theme}"]`
-    //   );
-    //   const svgOfActiveBtn = btnToActive
-    //     .querySelector("svg use")
-    //     .getAttribute("href");
-
-    //   document.querySelectorAll("[data-bs-theme-value]").forEach((element) => {
-    //     element.classList.remove("active");
-    //     element.setAttribute("aria-pressed", "false");
-    //   });
-
-    //   btnToActive.classList.add("active");
-    //   btnToActive.setAttribute("aria-pressed", "true");
-    //   activeThemeIcon.setAttribute("href", svgOfActiveBtn);
-    //   const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`;
-    //   themeSwitcher.setAttribute("aria-label", themeSwitcherLabel);
-
-    //   if (focus) {
-    //     themeSwitcher.focus();
-    //   }
-    // };
-
-    // window
-    //   .matchMedia("(prefers-color-scheme: dark)")
-    //   .addEventListener("change", () => {
-    //     const storedTheme = getStoredTheme();
-    //     if (storedTheme !== "light" && storedTheme !== "dark") {
-    //       setTheme(getPreferredTheme());
-    //     }
-    //   });
   })();
 
   return (
