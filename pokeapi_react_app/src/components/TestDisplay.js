6937964b02b19c4e00aa8fd230492032dc29df1d
igerrits13 <77903575+igerrits13@users.noreply.github.com>
2024-11-14 17:11:14 -0700

    Importing React-slick for testing

--- pokeapi_react_app/src/components/TestDisplay.js
+++ pokeapi_react_app/src/components/TestDisplay.js
@@ -1,4 +1,7 @@
 import { Link } from "react-router-dom";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const TestDisplay = () => {
   return (
