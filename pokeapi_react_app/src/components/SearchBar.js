4284c552f978f7ead72f430c024fd84e6d144fcf
igerrits13 <77903575+igerrits13@users.noreply.github.com>
2024-11-22 12:20:30 -0700

    Added and stylized search results dropdown

--- pokeapi_react_app/src/components/SearchBar.js
+++ pokeapi_react_app/src/components/SearchBar.js
@@ -1,9 +1,20 @@
 import SearchResults from "./SearchResults";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const SearchBar = () => {
   const [searchText, setSearchText] = useState("");
   const [isSearchHovered, setIsSearchHovered] = useState(false);
+  const [isSearchFocused, setIsSearchFocused] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
+
+  // Fetch the list of all pokémon species
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/?limit=5000`)
+      .then((response) => response.json())
+      .then((data) => {
+        setSearchResults(data.results);
+      });
+  }, []);
 
   // // Handle whether the search bar is being hovered over of not
   const handleSearchHover = () => {
@@ -12,6 +23,21 @@ const SearchBar = () => {
 
   const handleSearchUnHover = () => {
     setIsSearchHovered(false);
+    if (!isSearchFocused) {
+      clearSearchText();
+    }
+  };
+
+  // // Handle whether the search bar is being hovered over of not
+  const handleSearchFocus = () => {
+    setIsSearchFocused(true);
+  };
+
+  const handleSearchBlur = () => {
+    setIsSearchFocused(false);
+    if (!isSearchHovered) {
+      clearSearchText();
+    }
   };
 
   // Update the text in the search bar
@@ -19,38 +45,106 @@ const SearchBar = () => {
     setSearchText(e.target.value);
   };
 
+  // Clear the search text
+  const clearSearchText = () => {
+    setSearchText("");
+  };
+
   // Display the search bar if it is being hovered over of there is a text value in it
   const currSearch =
-    isSearchHovered || searchText ? "showSearch bg-secondary" : "bg-body ";
+    isSearchHovered || searchText || isSearchFocused
+      ? "bg-secondary showSearch"
+      : "bg-body ";
 
   return (
-    <div>
+    // <div>
+    //   <div
+    //     className={`rounded-pill mx-auto pokeSearch delay ${currSearch}`}
+    //     role="search"
+    //     onMouseEnter={handleSearchHover}
+    //     onMouseLeave={handleSearchUnHover}
+    //   >
+    //     <input
+    //       type="search"
+    //       placeholder="Search Pokémon"
+    //       value={searchText}
+    //       onChange={updateSearchText}
+    //       className="h-100 w-100 bg-secondary pokeSearchBar"
+    //       aria-label="Search Pokémon"
+    //     />
+    //     <div
+    //       className={"rounded-circle align-self-center bg-body searchButton"}
+    //     >
+    //       <i className="fa-solid fa-x searchIcon"></i>
+    //       <img
+    //         className="searchBall"
+    //         src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/dream-world/poke-ball.png"
+    //         alt="Pokéball Search Icon"
+    //         onClick={clearSearchText}
+    //       ></img>
+    //     </div>
+    //   </div>
+    //   <div
+    //     className="dropdown-menu"
+    //     onMouseEnter={handleSearchHover}
+    //     onMouseLeave={handleSearchUnHover}
+    //   >
+    //     {isSearchHovered || searchText ? (
+    //       <SearchResults
+    //         searchText={searchText}
+    //         searchResults={searchResults}
+    //       />
+    //     ) : (
+    //       <></>
+    //     )}
+    //   </div>
+    // </div>
+
+    <div className="dropdown mx-auto w-25">
       <div
-        className={`rounded-pill mx-auto pokeSearch delay ${currSearch}`}
+        className={`rounded-pill mx-auto pokeSearch delay ${
+          isSearchHovered || isSearchFocused ? "dropdown-toggle" : ""
+        } ${currSearch}`}
         role="search"
         onMouseEnter={handleSearchHover}
         onMouseLeave={handleSearchUnHover}
+        onFocus={handleSearchFocus}
+        onBlur={handleSearchBlur}
       >
         <input
-          type="search"
+          type="text"
           placeholder="Search Pokémon"
           value={searchText}
           onChange={updateSearchText}
-          className="h-100 w-100 rounded-pill bg-secondary pokeSearchBar"
-          aria-label="Search"
+          className="h-100 w-100 bg-secondary pokeSearchBar"
+          aria-label="Search Pokémon"
         />
         <div
           className={"rounded-circle align-self-center bg-body searchButton"}
         >
-          <i className="fa-solid fa-magnifying-glass searchIcon"></i>
+          <i className="fa-solid fa-x searchIcon"></i>
           <img
             className="searchBall"
             src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/dream-world/poke-ball.png"
             alt="Pokéball Search Icon"
+            onClick={clearSearchText}
           ></img>
         </div>
       </div>
-      {isSearchHovered || searchText ? <SearchResults /> : <></>}
+      {isSearchHovered || isSearchFocused ? (
+        <ul
+          className="dropdown-menu w-100 d-block mt-0"
+          onMouseEnter={handleSearchHover}
+          onMouseLeave={handleSearchUnHover}
+        >
+          <SearchResults
+            searchText={searchText}
+            searchResults={searchResults}
+          />
+        </ul>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
--- pokeapi_react_app/src/components/SearchResults.js
+++ pokeapi_react_app/src/components/SearchResults.js
@@ -1,25 +1,36 @@
 import { Link } from "react-router-dom";
 
-function SearchBar() {
-  return (
-    <div className="dataResult">
-      <div>
-        <Link to={"./"}>Item 1</Link>
-      </div>
-      <div>
-        <Link to={"./"}>Item 2</Link>
-      </div>
-      <div>
-        <Link to={"./"}>Item 3</Link>
-      </div>
-      <div>
-        <Link to={"./"}>Item 4</Link>
-      </div>
-      <div>
-        <Link to={"./"}>Item 5</Link>
-      </div>
-    </div>
-  );
+function SearchBar({ searchText, searchResults }) {
+  let searchHTML;
+
+  // Create search results for each pokemon species
+  if (searchText !== "") {
+    // searchHTML = searchResults
+    const searchResultsList = searchResults.filter((res) =>
+      res.name.includes(`${searchText}`)
+    );
+    if (Object.keys(searchResultsList).length === 0) {
+      searchHTML = <></>;
+    } else;
+    searchHTML = searchResultsList.map((obj, i) => {
+      return (
+        <Link className="dropdown-item" key={i} to="./">
+          {obj.name}
+        </Link>
+      );
+    });
+  } else {
+    searchHTML = searchResults.map((obj, i) => {
+      return (
+        <Link className="dropdown-item" key={i} to="./">
+          {obj.name}
+        </Link>
+      );
+    });
+  }
+
+  return <div className=" searchResults">{searchHTML.slice(0, 20)}</div>;
+  // return <></>;
 }
 
 export default SearchBar;
