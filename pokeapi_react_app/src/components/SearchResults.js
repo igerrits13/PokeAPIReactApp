be312fa35ccac9f32579c7842eb3ede039f1d848
igerrits13 <77903575+igerrits13@users.noreply.github.com>
2024-11-22 16:28:43 -0700

    Code cleanup

--- pokeapi_react_app/src/components/SearchResults.js
+++ pokeapi_react_app/src/components/SearchResults.js
@@ -40,50 +40,10 @@ function SearchBar({ searchText, searchResults }) {
           No Pokémon Found
         </Link>
       ) : (
-        // <div className="ms-3">No Pokémon Found</div>
         <div className="searchResults">{searchHTML.slice(0, 20)}</div>
       )}
     </div>
   );
-  // <div className="searchResults">{searchHTML.slice(0, 20)}</div>;
 }
 
 export default SearchBar;
-
-// import { Link } from "react-router-dom";
-
-// // Filter and display the search results based on search text
-// function SearchResults({ searchText, searchResults }) {
-//   let searchHTML;
-
-//   // Filter results if there is search text from the user
-//   if (searchText !== "") {
-//     // searchHTML = searchResults
-//     const searchResultsList = searchResults.filter((res) =>
-//       res.name.includes(`${searchText}`)
-//     );
-//     if (Object.keys(searchResultsList).length === 0) {
-//       searchHTML = <></>;
-//     } else
-//       searchHTML = searchResultsList.map((obj, i) => {
-//         return (
-//           <Link className="dropdown-item" key={i} to="./">
-//             {obj.name}
-//           </Link>
-//         );
-//       });
-//   } else {
-//     searchHTML = searchResults.map((obj, i) => {
-//       return (
-//         <Link className="dropdown-item" key={i} to="./">
-//           {obj.name}
-//         </Link>
-//       );
-//     });
-//   }
-
-//   return <div className=" searchResults">{searchHTML.slice(0, 20)}</div>;
-//   // return <></>;
-// }
-
-// export default SearchResults;
