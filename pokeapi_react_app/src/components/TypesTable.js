ad70659a849db3429d16e76c484516138623f1a0
Ian Gerrits <77903575+igerrits13@users.noreply.github.com>
2024-12-07 15:38:58 -0700

    Add buttons to type table

--- pokeapi_react_app/src/components/TypesTable.js
+++ pokeapi_react_app/src/components/TypesTable.js
@@ -1,108 +1,130 @@
-// import BugType from "./icons/bug.svg";
-// import DarkType from "./icons/dark.svg";
-// import DragonType from "./icons/dragon.svg";
-// import ElectricType from "./icons/electric.svg";
-// import FairyType from "./icons/fairy.svg";
-// import FightingType from "./icons/fighting.svg";
-// import FireType from "./icons/fire.svg";
-// import FlyingType from "./icons/flying.svg";
-// import GhostType from "./icons/ghost.svg";
-// import GrassType from "./icons/grass.svg";
-// import GroundType from "./icons/ground.svg";
-// import IceType from "./icons/ice.svg";
-// import NormalType from "./icons/normal.svg";
-// import PoisonType from "./icons/poison.svg";
-// import PsychicType from "./icons/psychic.svg";
-// import RockType from "./icons/rock.svg";
-// import SteelType from "./icons/steel.svg";
-// import WaterType from "./icons/water.svg";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import BugType from "./icons/bug.svg";
+import DarkType from "./icons/dark.svg";
+import DragonType from "./icons/dragon.svg";
+import ElectricType from "./icons/electric.svg";
+import FairyType from "./icons/fairy.svg";
+import FightingType from "./icons/fighting.svg";
+import FireType from "./icons/fire.svg";
+import FlyingType from "./icons/flying.svg";
+import GhostType from "./icons/ghost.svg";
+import GrassType from "./icons/grass.svg";
+import GroundType from "./icons/ground.svg";
+import IceType from "./icons/ice.svg";
+import NormalType from "./icons/normal.svg";
+import PoisonType from "./icons/poison.svg";
+import PsychicType from "./icons/psychic.svg";
+import RockType from "./icons/rock.svg";
+import SteelType from "./icons/steel.svg";
+import WaterType from "./icons/water.svg";
 
 const TypseTable = () => {
-  // Set the type attributes for the current buttoon
-  // function getTypeIcon(typeName) {
-  //   switch (typeName) {
-  //     case "bug":
-  //       typeStyle = "bugType";
-  //       return BugType;
-  //     case "dark":
-  //       typeStyle = "darkType";
-  //       return DarkType;
-  //     case "dragon":
-  //       typeStyle = "dragonType";
-  //       return DragonType;
-  //     case "electric":
-  //       typeStyle = "electricType";
-  //       return ElectricType;
-  //     case "fairy":
-  //       typeStyle = "fairyType";
-  //       return FairyType;
-  //     case "fighting":
-  //       typeStyle = "fightingType";
-  //       return FightingType;
-  //     case "fire":
-  //       typeStyle = "fireType";
-  //       return FireType;
-  //     case "flying":
-  //       typeStyle = "flyingType";
-  //       return FlyingType;
-  //     case "ghost":
-  //       typeStyle = "ghostType";
-  //       return GhostType;
-  //     case "grass":
-  //       typeStyle = "grassType";
-  //       return GrassType;
-  //     case "ground":
-  //       typeStyle = "groundType";
-  //       return GroundType;
-  //     case "ice":
-  //       typeStyle = "iceType";
-  //       return IceType;
-  //     case "normal":
-  //       typeStyle = "normalType";
-  //       return NormalType;
-  //     case "poison":
-  //       typeStyle = "poisonType";
-  //       return PoisonType;
-  //     case "psychic":
-  //       typeStyle = "psychicType";
-  //       return PsychicType;
-  //     case "rock":
-  //       typeStyle = "rockType";
-  //       return RockType;
-  //     case "steel":
-  //       typeStyle = "steelType";
-  //       return SteelType;
-  //     case "water":
-  //       typeStyle = "waterType";
-  //       return WaterType;
-  //     default:
-  //       return null;
-  //   }
-  // }
+  const [typesResults, setTypesResult] = useState([]);
+  let typeStyle = "";
+
+  // Fetch the types for the buttons
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/type/?limit=-1`)
+      .then((response) => response.json())
+      .then((data) => {
+        setTypesResult(data.results);
+      });
+  }, []);
+
+  // Set the type attributes for the current button
+  function getTypeIcon(typeName) {
+    switch (typeName) {
+      case "bug":
+        typeStyle = "bugType";
+        return BugType;
+      case "dark":
+        typeStyle = "darkType";
+        return DarkType;
+      case "dragon":
+        typeStyle = "dragonType";
+        return DragonType;
+      case "electric":
+        typeStyle = "electricType";
+        return ElectricType;
+      case "fairy":
+        typeStyle = "fairyType";
+        return FairyType;
+      case "fighting":
+        typeStyle = "fightingType";
+        return FightingType;
+      case "fire":
+        typeStyle = "fireType";
+        return FireType;
+      case "flying":
+        typeStyle = "flyingType";
+        return FlyingType;
+      case "ghost":
+        typeStyle = "ghostType";
+        return GhostType;
+      case "grass":
+        typeStyle = "grassType";
+        return GrassType;
+      case "ground":
+        typeStyle = "groundType";
+        return GroundType;
+      case "ice":
+        typeStyle = "iceType";
+        return IceType;
+      case "normal":
+        typeStyle = "normalType";
+        return NormalType;
+      case "poison":
+        typeStyle = "poisonType";
+        return PoisonType;
+      case "psychic":
+        typeStyle = "psychicType";
+        return PsychicType;
+      case "rock":
+        typeStyle = "rockType";
+        return RockType;
+      case "steel":
+        typeStyle = "steelType";
+        return SteelType;
+      case "water":
+        typeStyle = "waterType";
+        return WaterType;
+      default:
+        return null;
+    }
+  }
+
+    // Create buttons for each type
+    const typesHTML = typesResults.map((obj, i) => {
+      const typeIcon = getTypeIcon(obj.name);
+      if (obj.name !== "unknown" && obj.name !== "stellar") {
+        return (
+          <Link key={i} to="./">
+            <button className={`m-2 fs-6 fw-bold type-item ${typeStyle}`}>
+              <img
+                className="me-2 typeBtnImg"
+                // className="type-img"
+                src={typeIcon}
+                alt={`${obj.name} type icon`}
+              ></img>
+              {obj.name.toUpperCase()}
+            </button>
+          </Link>
+        );
+      } else {
+        return <div key={i}></div>;
+      }
+    });
+
 
   return (
-    <div className="types-table">
-      <button className="type-item">Item 1</button>
-      <button className="type-item">Item 2</button>
-      <button className="type-item">Item 3 bruh</button>
-      <button className="type-item">Item 4</button>
-      <button className="type-item">Item 5</button>
-      <button className="type-item">Item 6</button>
-      <button className="type-item">Item 7 with</button>
-      <button className="type-item">Item 8</button>
-      <button className="type-item">Item 9 with somoe</button>
-      <button className="type-item">Item 10</button>
-      <button className="type-item">Item 11</button>
-      <button className="type-item">Item 12</button>
-      <button className="type-item">Item 13 additional</button>
-      <button className="type-item">
-        Item 14 whole lot more text going on here
-      </button>
-      <button className="type-item">Item 15</button>
-      <button className="type-item">Item 16</button>
-      <button className="type-item">Item 17 text goes here</button>
-      <button className="type-item">18</button>
-      <button className="type-item">Item 19 even more </button>
+    <div>
+      <div className="sub-header">
+        Types
+      </div>
+      <div className="types-table">
+        {typesHTML}
+      </div>
     </div>
   );
 };
