904de6ba60aa6ad9b3052989043df466b7a0b8e0
igerrits13 <77903575+igerrits13@users.noreply.github.com>
2024-12-16 16:00:27 -0700

    Code cleanup

--- pokeapi_react_app/src/components/PokemonCard.js
+++ pokeapi_react_app/src/components/PokemonCard.js
@@ -3,23 +3,21 @@ import { Link } from "react-router-dom";
 // Create individual Pokémon cards
 const PokemonCard = ({ obj, i, pokeCountTotal }) => {
   const cardNum = "00" + i;
-  if (i < pokeCountTotal) {
-    return (
-      <Link className="pokemon-card">
-        <div className="pokemon-card-title">
-          {obj.name[0].toUpperCase() + obj.name.slice(1)}
-        </div>
-        <img
-          className="pokemon-image"
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${i}.png`}
-          alt={`${obj.name} card`}
-        />
-        <div className="pokemon-card-number">
-          #{cardNum.slice(cardNum.length - 4)}
-        </div>
-      </Link>
-    );
-  }
+  return (
+    <Link className="pokemon-card">
+      <div className="pokemon-card-title">
+        {obj.name[0].toUpperCase() + obj.name.slice(1)}
+      </div>
+      <img
+        className="pokemon-image"
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${i}.png`}
+        alt={`${obj.name} card`}
+      />
+      <div className="pokemon-card-number">
+        #{cardNum.slice(cardNum.length - 4)}
+      </div>
+    </Link>
+  );
 };
 
 export default PokemonCard;
