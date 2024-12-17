46ea506f32be7e2888f6b501d8fb78e36932feab
igerrits13 <77903575+igerrits13@users.noreply.github.com>
2024-12-17 12:21:33 -0700

    Style search bar and active/hovering logic

--- pokeapi_react_app/src/components/SearchBar.js
+++ pokeapi_react_app/src/components/SearchBar.js
@@ -1,14 +1,65 @@
+import { useState } from "react";
+
 // Search bar for searching Pokémon
 const SearchBar = () => {
+  const [searchText, setSearchText] = useState("");
+  const [searchBarActive, setSearchBarActive] = useState(false);
+  const [searchBarFocus, setSearchBarFocus] = useState(false);
+
+  // Update the text in the search bar
+  const updateSearchText = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  // Update state for when the search bar is being hovered
+  const handleMouseEnter = () => {
+    setSearchBarActive(true);
+  };
+
+  const handleMouseLeave = () => {
+    setSearchBarActive(false);
+  };
+
+  // Update state for when the search bar is being focused
+  const handleOnFocus = () => {
+    setSearchBarFocus(true);
+  };
+
+  const handleOnBlur = () => {
+    setSearchBarFocus(false);
+    setSearchBarActive(false);
+    clearSearchText();
+  };
+
+  // Clear search text when not hovered or focused
+  const clearSearchText = () => {
+    setSearchText("");
+  };
+
   return (
-    <form>
-      <input
-        className="searchbar-input"
-        type="search"
-        placeholder="Search Pokémon . . ."
-      />
-      <i className="fa-solid fa-magnifying-glass searchbar-icon"></i>
-    </form>
+    <div>
+      <form
+        className={`searchbar-container ${
+          searchBarActive || searchText !== "" || searchBarFocus
+            ? "searchbar-active"
+            : ""
+        }`}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        onFocus={handleOnFocus}
+        onBlur={handleOnBlur}
+      >
+        <input
+          className="searchbar-input"
+          type="search"
+          value={searchText}
+          onChange={updateSearchText}
+          placeholder="Search Pokémon . . ."
+          aria-label="Search Pokémon"
+        />
+        <i className="fa-solid fa-magnifying-glass searchbar-icon"></i>
+      </form>
+    </div>
   );
 };
 
