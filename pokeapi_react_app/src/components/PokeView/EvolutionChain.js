370e75a8e14494aefc43bda66e8eab3220d3b4e2
igerrits13 <77903575+igerrits13@users.noreply.github.com>
2025-02-09 01:25:46 -0700

    Code cleanup

--- pokeapi_react_app/src/components/PokeView/EvolutionChain.js
+++ pokeapi_react_app/src/components/PokeView/EvolutionChain.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import PokemonCard from "../CommonComponents/PokemonCard";
 
@@ -39,17 +39,6 @@ const EvolutionChain = ({ pokeChainURL, screenSize, isDarkMode }) => {
     fetchData();
   }, [pokeChainURL]);
 
-  const flexboxRef = useRef(null);
-
-  // useEffect(() => {
-  //   if (!isPokeChainLoading) {
-  //     // Adjust scroll position to start in the middle after the component has mounted
-  //     const flexbox = flexboxRef.current;
-  //     const middleScrollPosition = flexbox.scrollHeight / 2.5;
-  //     flexbox.scrollTop = middleScrollPosition;
-  //   }
-  // }, [isPokeChainLoading]);
-
   // If the API call returns an error, navigate to the page not found
   // (Redundant to inside fetch call to avoid compilation error)
   if (error) {
@@ -66,16 +55,6 @@ const EvolutionChain = ({ pokeChainURL, screenSize, isDarkMode }) => {
   };
 
   const getEvolutions = (evolutions) => {
-    // console.log(evolutions);
-    //   <div
-    //     className={`evolution-chain-section-horizontal ${
-    //       Object.entries(pokeChainData.chain.evolves_to).length === 1
-    //         ? "flex-centered"
-    //         : ""
-    //     }
-    // }`}
-    //     // ref={flexboxRef}
-    //   >
     return evolutions.map((evolution, i) => {
       console.log(evolution);
       return (
@@ -90,42 +69,12 @@ const EvolutionChain = ({ pokeChainURL, screenSize, isDarkMode }) => {
               />
             </div>
           </div>
-          {/* {console.log("Before recusive")}
-              {console.log(Object.entries(evolution[1].evolves_to))} */}
           {evolution[1].evolves_to &&
             getEvolutions(Object.entries(evolution[1].evolves_to))}
         </div>
       );
     });
   };
-
-  // const getEvolutions = (evolutions) => {
-  //   return evolutions.map((evolution, i) => {
-  //     return (
-  //       <div className="evolution-chain-icon-row" key={i}>
-  //         <i className="fa-solid fa-arrow-right-long" />
-  //         <div className="evolution-chain-card">
-  //           <PokemonCard
-  //             obj={evolution[1].species}
-  //             i={getPokeNum(evolution[1].species.url)}
-  //             isDarkMode={isDarkMode}
-  //           />
-  //         </div>
-  //         {/* Recursively call getEvolutions if there is a next evolution */}
-  //         {evolution.evolves_to &&
-  //           getEvolutions(Object.entries(evolution.evolves_to))}
-  //       </div>
-  //     );
-  //   });
-  // };
-
-  // <div className="evolution-chain-card" key={i}>
-  //   <PokemonCard
-  //     obj={evolution[1].species}
-  //     i={getPokeNum(evolution[1].species.url)}
-  //     isDarkMode={isDarkMode}
-  //   />
-  // </div>
 
   return (
     <>
@@ -152,7 +101,6 @@ const EvolutionChain = ({ pokeChainURL, screenSize, isDarkMode }) => {
                 : ""
             }
           }`}
-            ref={flexboxRef}
           >
             {getEvolutions(Object.entries(pokeChainData.chain.evolves_to))}
           </div>
@@ -163,62 +111,3 @@ const EvolutionChain = ({ pokeChainURL, screenSize, isDarkMode }) => {
 };
 
 export default EvolutionChain;
-
-// const genHTML = genResults.map((obj, i) => {
-//   return (
-//     <option key={i} className={`${optionStyle}`} value={`${i + 1}`}>
-//       {formattedGenName}
-//     </option>
-//   );
-// });
-
-// {getEvolutions(Object.entries(pokeChainData.chain.evolves_to))}
-
-// const evoHTML = !isPokeChainLoading && (
-//   <div className={`evolution-chain-container-horizontal ${fontStyle}`}>
-//     <div className="evolution-chain-card">
-//       <PokemonCard
-//         obj={pokeChainData.chain.species}
-//         i={getPokeNum(pokeChainData.chain.species.url)}
-//         isDarkMode={isDarkMode}
-//       />
-//     </div>
-//     <div className="evolution-chain-icon-row">
-//       {/* {console.log(
-//         getEvolutions(Object.entries(pokeChainData.chain.evolves_to))
-//       )} */}
-//       {getEvolutions(Object.entries(pokeChainData.chain.evolves_to))}
-//     </div>
-//     {/* {pokeChainData.chain.evolves_to[0] !== undefined && (
-//       <div className="evolution-chain-section-horizontal">
-// <div className="evolution-chain-icon-row">
-//   <i className="fa-solid fa-arrow-right-long" />
-//   <div className="evolution-chain-card">
-//     <PokemonCard
-//       obj={pokeChainData.chain.evolves_to[0].species}
-//       i={getPokeNum(pokeChainData.chain.evolves_to[0].species.url)}
-//       isDarkMode={isDarkMode}
-//     />
-//   </div>
-// </div>
-//       </div>
-//     )}
-//     {pokeChainData.chain.evolves_to[0] !== undefined &&
-//       pokeChainData.chain.evolves_to[0].evolves_to[0] !== undefined && (
-//         <div className="evolution-chain-section-horizontal">
-//           <div className="evolution-chain-icon-row">
-//             <i className="fa-solid fa-arrow-right-long"></i>
-//             <div className="evolution-chain-card">
-//               <PokemonCard
-//                 obj={pokeChainData.chain.evolves_to[0].evolves_to[0].species}
-//                 i={getPokeNum(
-//                   pokeChainData.chain.evolves_to[0].evolves_to[0].species.url
-//                 )}
-//                 isDarkMode={isDarkMode}
-//               />
-//             </div>
-//           </div>
-//         </div>
-//       )} */}
-//   </div>
-// );
