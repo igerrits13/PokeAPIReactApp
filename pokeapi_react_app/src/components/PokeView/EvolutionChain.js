f3fe9a518f040b09fcc34c97d3de619bc2e66d3e
igerrits13 <77903575+igerrits13@users.noreply.github.com>
2025-02-10 22:54:57 -0700

    Working on adding evolution details

--- pokeapi_react_app/src/components/PokeView/EvolutionChain.js
+++ pokeapi_react_app/src/components/PokeView/EvolutionChain.js
@@ -54,6 +54,189 @@ const EvolutionChain = ({ pokeChainURL, screenSize, isDarkMode }) => {
     return parseInt(urlNumber, 10);
   };
 
+  // Map to match for each possible type case
+  // const evoMapping = {
+  //   gender: [BugType, "bug-type"],
+  //   held_item: [DarkType, "dark-type"],
+  //   item: [DragonType, "dragon-type"],
+  //   known_move: [ElectricType, "electric-type"],
+  //   known_move_type: [FairyType, "fairy-type"],
+  //   location: [FightingType, "fighting-type"],
+  //   min_affection: [FireType, "fire-type"],
+  //   min_beauty: [FlyingType, "flying-type"],
+  //   min_happiness: [GhostType, "ghost-type"],
+  //   min_level: [GrassType, "grass-type"],
+  //   needs_overworld_rain: [GroundType, "ground-type"],
+  //   party_species: [IceType, "ice-type"],
+  //   party_type: [NormalType, "normal-type"],
+  //   relative_physical_stats: [PoisonType, "poison-type"],
+  //   time_of_day: [PsychicType, "psychic-type"],
+  //   trade_species: [RockType, "rock-type"],
+  //   turn_upside_down: [SteelType, "steel-type"],
+  //   trigger: [WaterType, "water-type"],
+  // };
+
+  // Map to match for each possible evolution trigger case
+  const getTrigger = (evolutionDetails) => {
+    let evoArr = Array.from(Object.entries(evolutionDetails)).filter(
+      (item) => item[0] !== "trigger"
+    );
+
+    switch (evolutionDetails.trigger.name) {
+      case "level-up":
+        return evolutionDetails.min_level
+          ? [
+              `Reach level ${evolutionDetails.min_level}`,
+              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/rare-candy.png",
+              evoArr.filter((item) => item[0] !== "min_level"),
+            ]
+          : ["Level up", [], evoArr];
+      case "trade":
+        return ["Trade for", [], evoArr];
+      case "use-item":
+        return ["Use", [], evoArr];
+      case "shed":
+        return ["Shed", [], evoArr];
+      case "spin":
+        return ["Spin", [], evoArr];
+      case "tower-of-darkness":
+        return ["Tower of Darkness", [], evoArr];
+      case "tower-of-waters":
+        return ["Towers of Waters", [], evoArr];
+      case "three-critical-hits":
+        return [
+          "Land 3 critical hits with Pokémon in single battle",
+          [],
+          evoArr,
+        ];
+      case "take-damage":
+        return ["Take damage", [], evoArr];
+      case "other":
+        return ["Other", [], evoArr];
+      case "agile-style-move":
+        return ["Agile moves", [], evoArr];
+      case "strong-style-move":
+        return ["Strong moves", [], evoArr];
+      case "recoil-damage":
+        return ["Recoil damage", [], evoArr];
+      default:
+        return [`Not found: ${evolutionDetails.trigger.name}`, [], evoArr];
+    }
+    // held_item: [DarkType, "dark-type"],
+    // item: [DragonType, "dragon-type"],
+    // known_move: [ElectricType, "electric-type"],
+    // known_move_type: [FairyType, "fairy-type"],
+    // location: [FightingType, "fighting-type"],
+    // min_affection: [FireType, "fire-type"],
+    // min_beauty: [FlyingType, "flying-type"],
+    // min_happiness: [GhostType, "ghost-type"],
+    // min_level: [GrassType, "grass-type"],
+    // needs_overworld_rain: [GroundType, "ground-type"],
+    // party_species: [IceType, "ice-type"],
+    // party_type: [NormalType, "normal-type"],
+    // relative_physical_stats: [PoisonType, "poison-type"],
+    // time_of_day: [PsychicType, "psychic-type"],
+    // trade_species: [RockType, "rock-type"],
+    // turn_upside_down: [SteelType, "steel-type"],
+    // trigger: [WaterType, "water-type"],
+  };
+
+  const genderMapping = {
+    0: "Male",
+    1: "Female",
+    2: "Genderless",
+  };
+
+  const getEvoDetails = (evolutionDetails) => {
+    let detailsArr = [];
+    Object.entries(evolutionDetails).forEach((detail, i) => {
+      if (detail[1][1] !== null && detail[1][1] !== undefined) {
+        console.log("Keeping");
+        console.log(detail);
+      } else {
+        console.log("Skipping");
+        console.log(detail);
+      }
+      // if (evolutionDetails.gender !== null) {
+      //   detailsArr.push(`(${genderMapping[evolutionDetails.gender]} only)`);
+      // } else {
+      //   detailsArr.push("None");
+      // }
+    });
+    // console.log(detailsArr);
+    return [detailsArr];
+
+    // held_item: [DarkType, "dark-type"],
+    // item: [DragonType, "dragon-type"],
+    // known_move: [ElectricType, "electric-type"],
+    // known_move_type: [FairyType, "fairy-type"],
+    // location: [FightingType, "fighting-type"],
+    // min_affection: [FireType, "fire-type"],
+    // min_beauty: [FlyingType, "flying-type"],
+    // min_happiness: [GhostType, "ghost-type"],
+    // min_level: [GrassType, "grass-type"],
+    // needs_overworld_rain: [GroundType, "ground-type"],
+    // party_species: [IceType, "ice-type"],
+    // party_type: [NormalType, "normal-type"],
+    // relative_physical_stats: [PoisonType, "poison-type"],
+    // time_of_day: [PsychicType, "psychic-type"],
+    // trade_species: [RockType, "rock-type"],
+    // turn_upside_down: [SteelType, "steel-type"],
+  };
+
+  const getTriggerEvent = (evolution) => {
+    // console.log(Object.entries(evolution.evolution_details)[0][1]);
+    // if (
+    //   Object.entries(evolution.evolution_details)[0][1].trigger.name ===
+    //     "level-up" &&
+    //   Object.entries(evolution.evolution_details)[0][1].min_level
+    // ) {
+    //   return (
+    //     <div className="evolution-chain-section-vertical">
+    //       Reach level{" "}
+    //       {Object.entries(evolution.evolution_details)[0][1].min_level}
+    //       <img
+    //         className="evolution-chain-image"
+    //         src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/rare-candy.png"
+    //         alt="Rare Candy"
+    //       />
+    //     </div>
+    //   );
+    // }
+    // let evoArr,
+    //   iconsArr = [];
+
+    // Get the trigger string, array of details to still look through and icon URL of the current evolution chain link
+    const [evoTrigger, iconURL, evoArr] = getTrigger(
+      Object.entries(evolution.evolution_details)[0][1]
+    );
+
+    // console.log(iconURL);
+
+    const [evoTriggerArr, iconURLArr] = getEvoDetails(evoArr);
+
+    // Iterate over the other items within the array and form sentences and icons
+
+    // const []
+
+    // console.log(Object.entries(evolution.evolution_details)[0][1]);
+
+    evoTriggerArr.unshift(evoTrigger);
+
+    return (
+      <div className="evolution-chain-section-vertical evolution-chain-section-details">
+        {evoTriggerArr}
+        {iconURL[0] !== undefined && (
+          <img
+            className="evolution-chain-image"
+            src={iconURL}
+            alt="Rare Candy"
+          />
+        )}
+      </div>
+    );
+  };
+
   const getEvolutions = (evolutions) => {
     return evolutions.map((evolution, i) => {
       return (
@@ -67,6 +250,8 @@ const EvolutionChain = ({ pokeChainURL, screenSize, isDarkMode }) => {
           key={i}
         >
           <div className="evolution-chain-icon-row">
+            {Object.entries(evolution[1].evolution_details)[0] &&
+              getTriggerEvent(evolution[1])}
             <i className="fa-solid fa-arrow-right-long" />
             <div className="evolution-chain-card">
               <PokemonCard
