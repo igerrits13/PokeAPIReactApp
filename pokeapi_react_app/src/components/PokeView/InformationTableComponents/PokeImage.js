eb3dae06a7f29542c1676d0d78a401af9cd42fa7
igerrits13 <77903575+igerrits13@users.noreply.github.com>
2025-03-03 17:36:40 -0700

    Display message if artwork does not exist for Pokémon image

--- pokeapi_react_app/src/components/PokeView/InformationTableComponents/PokeImage.js
+++ pokeapi_react_app/src/components/PokeView/InformationTableComponents/PokeImage.js
@@ -3,17 +3,21 @@ const PokeImage = ({ pokeData, whosThatPokemon, isNormalToggle }) => {
   // Display the normal or shiny image of the current Pokémon
   return (
     <div className="pokeview-image-container">
-      <img
-        className={`pokeview-image ${
-          whosThatPokemon ? "pokeview-image-dark" : ""
-        }`}
-        src={
-          isNormalToggle
-            ? pokeData.sprites.other["official-artwork"].front_default
-            : pokeData.sprites.other["official-artwork"].front_shiny
-        }
-        alt={`${pokeData.species.name}`}
-      />
+      {pokeData.sprites.other["official-artwork"].front_default !== null ? (
+        <img
+          className={`pokeview-image ${
+            whosThatPokemon ? "pokeview-image-dark" : ""
+          }`}
+          src={
+            isNormalToggle
+              ? pokeData.sprites.other["official-artwork"].front_default
+              : pokeData.sprites.other["official-artwork"].front_shiny
+          }
+          alt={`${pokeData.species.name}`}
+        />
+      ) : (
+        <div className="pokeview-image">No image found!</div>
+      )}
     </div>
   );
 };
